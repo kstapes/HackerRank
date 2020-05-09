a84732673ac854c223324bdbb9ab78d7a592149d
kstapes <46645798+kstapes@users.noreply.github.com>
2020-05-09 09:43:33 +0100

    adding day 2 solution

---
+++ Day 2  Operators/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2__Operators
+{
+    class Program
+    {
+        static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            double tip = Convert.ToDouble(meal_cost * tip_percent / 100);
+            double tax = Convert.ToDouble(meal_cost * tax_percent / 100);
+            double total_cost = meal_cost + tip + tax;
+
+            Console.WriteLine(Math.Round(total_cost));
+        }
+        static void Main(string[] args)
+        {
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+            solve(meal_cost, tip_percent, tax_percent);
+        }
+    }
+}
