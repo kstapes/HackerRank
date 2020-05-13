a51e13e6d0815fe558053280d7bc02273c4888cc
kstapes <46645798+kstapes@users.noreply.github.com>
2020-05-13 14:08:24 +0100

    Day 7 & Day 8 Commit

---
+++ Day 6 Lets Review/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_6_Lets_Review
+{
+    class Program
+    {
+        public static string firstWord = "";
+        public static string secondWord = "";
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!input.All(char.IsDigit) && i % 2 == 0)
+                {
+                    firstWord += input[i];
+                }
+                else if (!input.All(char.IsDigit) && i % 2 != 0)
+                {
+                    secondWord += input[i];
+                }
+            }
+            Console.WriteLine(firstWord + " " + secondWord);
+        }
+    }
+}
