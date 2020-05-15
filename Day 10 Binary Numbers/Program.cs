ca0e65b59e78de10665fb135047ed8a8cbd2c383
kstapes <46645798+kstapes@users.noreply.github.com>
2020-05-15 16:45:55 +0100

    Day 10 Binary Numbers

---
+++ Day 10 Binary Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_10_Binary_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            //Converting an int to a string in the following method converts the int to binary 
+            string binNumber = Convert.ToString(n, 2);
+            int totalConseqNum = 0;
+
+            //split the binary string on 0
+            string[] binArray = binNumber.Split('0');
+
+            for (int i = 0; i < binArray.Length; i++)
+            {
+                //if the totalConseqNum is less than the length of the element
+                //in the array then set totalConseqNum to that length value
+                if (totalConseqNum < binArray[i].Length)
+                {
+                    totalConseqNum = binArray[i].Length;
+                }
+                
+            }
+            Console.WriteLine(totalConseqNum);
+            Console.ReadLine();
+        }
+    }
+}
